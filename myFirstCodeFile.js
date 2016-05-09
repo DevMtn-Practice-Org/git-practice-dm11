af57f0b044d7e29a20b99e21e15c67164b4da847
Jeremy Robertson <sparticus7n7@yahoo.com>
2016-05-09 14:54:51 -0600

    Fixes and stuf

--- myFirstCodeFile.js
+++ myFirstCodeFile.js
@@ -1,9 +1,11 @@
 var dm11 = "Started today!";
 var foo = "fighters!";
 var bar = "car";
+var animal = "cat";
 
 var journal = [];
 
 journal.push(dm11); 
 journal.push(foo);
 journal.push(bar);
+joural.push(animal);
